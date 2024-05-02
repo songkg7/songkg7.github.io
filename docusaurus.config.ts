c875a35cef4c8e211e659bb598ea722c2ab3b5d3
haril song <songkg7@gmail.com>
2024-05-03 01:16:39 +0900

    update footer items

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -123,16 +123,12 @@ const config: Config = {
           title: 'Community',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
               label: 'Discord',
               href: 'https://discordapp.com/invite/docusaurus',
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: 'LinkedIn',
+              href: 'https://linkedin.com/in/songkg7/',
             },
           ],
         },
@@ -150,7 +146,7 @@ const config: Config = {
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Haril Song, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
