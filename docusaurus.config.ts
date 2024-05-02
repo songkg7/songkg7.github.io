d858f2fc18fd7e486a400f1f2e832fa4eef47066
haril song <songkg7@gmail.com>
2024-05-03 00:40:32 +0900

    add katex

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 const config: Config = {
   title: 'Haril\'s Devlog',
@@ -35,6 +37,17 @@ const config: Config = {
   },
   themes: ['@docusaurus/theme-mermaid'],
 
+  // katex
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+
   presets: [
     [
       'classic',
@@ -56,6 +69,8 @@ const config: Config = {
           blogDescription: 'A blog about software development, programming, and computer science.',
           blogSidebarTitle: 'All posts',
           blogSidebarCount: 'ALL',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
