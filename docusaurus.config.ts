3d3217193866af5ab1f38a921cec72ba5a6fac5f
kgsong <kyunggeun.song@seavantage.com>
2024-05-03 20:17:38 +0900

    feat: add code highlight java and bash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -163,6 +163,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'bash'],
     },
   } satisfies Preset.ThemeConfig,
 };
