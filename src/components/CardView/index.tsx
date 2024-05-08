addb13fa6307909c6cd54cff24b0491df324862f
haril song <songkg7@gmail.com>
2024-05-08 17:37:21 +0900

    feat: root page design

---
+++ src/components/CardView/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Heading from "@theme/Heading";
+
+function Card() {
+    return (
+        <div className="card col col--4 row--align-center">
+            <div className={"card__image"}>
+                image
+            </div>
+            <div className="card__header">
+                <Heading as={"h1"}>Card Header</Heading>
+            </div>
+            <div className={"card__body"}>
+                <p>Card summary</p>
+            </div>
+        </div>
+    )
+}
+
+// front matter 에 pin:true 가 있을 경우 메인에 표시
+export default function CardView(): React.JSX.Element {
+
+    return (
+        <div className="container">
+            <div className="row">
+                {Card()}
+                {Card()}
+                {Card()}
+                {Card()}
+            </div>
+        </div>
+    );
+}
